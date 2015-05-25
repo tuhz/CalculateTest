b92631aaf2656756a60e8074b1ec540a98eb8204
洪智 <hongzhi@tju.edu.cn>
2015-05-25 19:58:34 +0800

    Calculate JUnit Test

---
+++ src/cn/edu/tju/scs/hongzhi/calculate/Calculate.java
@@ -0,0 +1,21 @@
+package cn.edu.tju.scs.hongzhi.calculate;
+
+public class Calculate {
+	
+	public static int add (int a, int b) {
+		return a+b;
+	}
+	
+	public static int substract (int a, int b) {
+		return a-b;
+	}
+	
+ 	public static int multiply (int a, int b) {
+ 		return a*b;
+ 	}
+ 	
+ 	public static float divide(int a, int b) {
+ 		return ((float)a)/((float)b);
+ 	}
+ 	
+}
---
+++ src/cn/edu/tju/scs/hongzhi/calculate/CalculateTest.java
@@ -0,0 +1,43 @@
+package cn.edu.tju.scs.hongzhi.calculate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+	int[] a = {-1, 1, 0, Integer.MIN_VALUE, Integer.MAX_VALUE};
+	int[] b = {1, 1, -1, -2};
+	long[] expAdd = {0, 2, 1, Integer.MIN_VALUE+1, Integer.MAX_VALUE+1};
+	long[] expSub = {-2, 0, -1, Integer.MIN_VALUE-1, Integer.MAX_VALUE-1};
+	long[] expMul = {1, -1, 0, Integer.MIN_VALUE*-1, Integer.MAX_VALUE*-1};
+ 	
+	@Test
+	public void testAdd() {
+		for (int i = 0; i< 5; i++) {
+			assertEquals((long)a[i]+b[0], Calculate.add(a[i], b[0]));
+		}
+	}
+
+	@Test
+	public void testSubstract() {
+		for (int i = 0; i< 5; i++) {
+			assertEquals((long)a[i]-b[1], Calculate.substract(a[i], b[1]));
+		}
+	}
+
+	@Test
+	public void testMultiply() {
+		for (int i = 0; i< 5; i++) {
+			assertEquals((long)a[i]*b[2], Calculate.multiply(a[i], b[2]));
+		}
+	}
+
+	@Test
+	public void testDivide() {
+		for (int i = 0; i< 5; i++) {
+			assertEquals((double)a[i]/b[3], Calculate.divide(a[i], b[3]));
+		}
+	}
+
+}
